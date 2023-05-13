6ce78ad92694b9c36e7be08e07ca1455e5118ee5
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-12 22:23:41 -0500

    Update two.py

--- name_and_main/two.py
+++ name_and_main/two.py
@@ -1,1 +1,10 @@
 #two.py
+import one 
+print("Top level in two.py")
+
+one.func()
+
+if __name__ == "__main__":
+    print("Two.py is being run directly")
+else:
+    print("Two.py is being imported")
