5ae2cfd9a090a64f506fb658eb11731ce84aef0e
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-06-21 21:21:11 -0500

    blogpost

--- 09_Final_capstone/app.py
+++ 09_Final_capstone/app.py
@@ -1,8 +1,6 @@
 from data_science_meetup_blog import app
+ 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
+    app.run(port=8080)
--- 09_Final_capstone/data_science_meetup_blog/blog_posts/views.py
+++ 09_Final_capstone/data_science_meetup_blog/blog_posts/views.py
@@ -1,3 +1,4 @@
+from os import abort
 from flask import render_template, request, Blueprint, url_for, redirect, flash 
 from flask_login import current_user, login_required
 from data_science_meetup_blog import db
@@ -5,6 +6,25 @@ from data_science_meetup_blog.models import BlogPost
 from data_science_meetup_blog.blog_posts.forms import BlogPostForm
 
 blog_posts = Blueprint('blog_posts', __name__)
+
+class Error(Exception):
+    """Base class for custom errors."""
+    pass
+
+
+class BlogPostCreationError(Error):
+    """Error raised when there is an issue with creating a blog post."""
+    pass
+
+
+class BlogPostUpdateError(Error):
+    """Error raised when there is an issue with updating a blog post."""
+    pass
+
+
+class BlogPostDeletionError(Error):
+    """Error raised when there is an issue with deleting a blog post."""
+    pass
 
 
 # CREATE
@@ -28,3 +48,56 @@ def create_post():
         flash("Error: Blog Post Not Created")
 
         return render_template('create_post.html', form=form)
+    
+
+    # BLOG POST (VIEW)  
+    @blog_posts.route('/<int:blog_post_id>')
+    def blog_post(blog_post_id):
+    # get blog post by id or return 404
+        blog_post = BlogPost.query.get_or_404(blog_post_id)
+        return render_template(
+            'blog_post.html', 
+            title=blog_post.title,
+            date=blog_post.date, 
+            post=blog_post
+        )
+# UPDATE
+@blog_post.route('/<int:blog_post_id/update',methods=['GET','POST'])
+@login_required
+def update(blog_post_id):
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+
+    if blog_post.author != current_user:
+        abort(403)
+
+    form = BlogPostForm()
+
+
+    if form.validate_on_submit():
+
+        blog_post.title = form.title.data
+        blog_post.text = form.text.data
+        db.session.commit()
+        flash('Blog Post Updated')
+        return redirect(url_for('blog_posts.blog_post',blog_post_id=blog_post.id))
+
+    elif request.method = 'GET':
+        form.title.data = blog_post.title
+        form.text.data = blog_post.text
+
+    return render_template('create_post.html',title='Updating',form=form)
+
+
+# DELETE
+@blog_post.route('/<int:blog_post_id/delete',methods=['GET','POST'])
+@login_required
+def delete_post(blog_post_id):
+
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+    if blog_post.author != current_user:
+        abort(403)
+
+    db.session.delete(blog_post)
+    db.session.commit()
+    flash('Blog Post Deleted')
+    return redirect(url_for('core.index'))
